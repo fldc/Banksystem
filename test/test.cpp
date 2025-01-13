f1211488f408bec5516a11b36d07ff945661b327
LindeborgLove <lindeborglove@gmail.com>
2025-01-13 10:58:59 +0100

    Created tests for BankAccount class and Bank class

--- test/test.cpp
+++ test/test.cpp
@@ -1,1 +1,35 @@
 #include <catch2/catch_test_macros.hpp>
+#include "BankAccount.h" // Not yet implemented
+
+TEST_CASE("BankAccount class", "[bankaccount],[class]")
+{
+    BankAccount bankAccount(600, 1);
+
+    REQUIRE_THROWS(bankAccount.withdraw(-1));
+    REQUIRE_THROWS(bankAccount.deposit(-1));
+
+    REQUIRE_THROWS(bankAccount.withdraw(-1500));
+    REQUIRE_THROWS(bankAccount.deposit(-1500));
+
+    REQUIRE_THROWS(bankAccount.withdraw(0));
+    REQUIRE_THROWS(bankAccount.deposit(0));
+
+    REQUIRE(bankAccount.getBalance() == 600);
+
+    bankAccount.withdraw(300);
+    REQUIRE(bankAccount.getBalance() == 300);
+
+    bankAccount.deposit(150);
+    REQUIRE(bankAccount.getBalance() == 450);
+}
+
+TEST_CASE("Bank class", "[bank],[class]")
+{
+    Bank bank;
+    BankAccount bankAccount(600, 1);
+
+    REQUIRE_NOTHROW(bank.addAccount(1, bankAccount));
+    
+    BankAccount &account;
+    REQUIRE_NOTHROW(account = bank.getAccount(1));
+}
